3247d9a91fdf5ab0c6c18ab2254c20ca69174c80
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-10-18 15:05:16 +0300

    first_part

--- petstagram/petstagram/settings.py
+++ petstagram/petstagram/settings.py
@@ -30,6 +30,13 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+MY_APPS = [
+    'petstagram.accounts',
+    'petstagram.pets',
+    'petstagram.common',
+    'petstagram.photos',
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+] + MY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -75,9 +82,13 @@ WSGI_APPLICATION = 'petstagram.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "petstagram",
+        "USER": "postgres-user",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
